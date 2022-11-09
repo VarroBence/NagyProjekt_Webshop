f58a8976cc523c4d747c0c619cbcc2bdae6f3908
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2022-11-09 21:12:46 +0100

    Laravel - update Listing.php

--- Laravel/myapp/app/Models/Listing.php
+++ Laravel/myapp/app/Models/Listing.php
@@ -1,46 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem Ipsum is simply dummy text of
-                 the printing and typesetting industry.
-                 Lorem Ipsum has been the industrys standard
-                 dummy text ever since the 1500s,
-                 when an unknown printer took a
-                 galley of type and scrambled it to make
-                 a type specimen book. '
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem Ipsum is simply dummy text of
-                 the printing and typesetting industry.
-                 Lorem Ipsum has been the industrys standard
-                 dummy text ever since the 1500s,
-                 when an unknown printer took a
-                 galley of type and scrambled it to make
-                 a type specimen book. '
-
-            ]
-            ];
-    }
-
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
-
-
-?>
