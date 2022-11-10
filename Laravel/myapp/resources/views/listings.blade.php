40e6c0ca73336148ae2902d286c838fd6c58e383
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2022-11-10 21:49:43 +0100

    listingS Tagek

--- Laravel/myapp/resources/views/listings.blade.php
+++ Laravel/myapp/resources/views/listings.blade.php
@@ -22,8 +22,31 @@
                 <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{$listing->price}} Ft</div>
+            <ul class="flex">
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">T-shirt</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Black</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Sport</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Latest</a>
+                </li>
+            </ul>
+
             <div class="text-lg mt-4">
-                <i class="fa-solid fa-location-dot"></i> 
+                <i class="fa-solid fa-shirt"></i>
             {{$listing->description}}
             </div>
         </div>
