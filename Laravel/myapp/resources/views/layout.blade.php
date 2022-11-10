2e4ed357c6dec14bbda515e0c681685758625997
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2022-11-10 11:13:41 +0100

    Laravel - Hero megírása

--- Laravel/myapp/resources/views/layout.blade.php
+++ Laravel/myapp/resources/views/layout.blade.php
@@ -24,12 +24,12 @@
                 },
             };
         </script>
-        <title>LaraGigs | Find Laravel Jobs & Projects</title>
+        <title>Webshop | Find T-shirts for your taste</title>
     </head>
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
             <a href="index.html"
-                ><img class="w-24" src="images/logo.jpg" alt="" class="logo"
+                ><img class="w-24" src="{{asset('images/logo.jpg')}}" alt="" class="logo"
             /></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 <li>
