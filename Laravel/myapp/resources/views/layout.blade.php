e30ceeb629dc76fb1760e4c856fce603c050e829
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2022-11-24 20:25:31 +0100

    layoutban átírva a logout
    dropdownt használt eredetileg, de nem működött, sima list item lett

--- Laravel/myapp/resources/views/layout.blade.php
+++ Laravel/myapp/resources/views/layout.blade.php
@@ -45,8 +45,8 @@
                         </li>
                     @endif
                 @else
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
@@ -55,7 +55,7 @@
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
-                        </div>
+                        </li>
                 @endguest
             </ul>
         </nav>
