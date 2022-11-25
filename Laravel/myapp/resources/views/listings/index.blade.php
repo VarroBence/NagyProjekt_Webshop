882d82d40f5ea2de5779db61fa0a075811ee6659
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2022-11-25 19:03:21 +0100

    Laravel - Pagination megvalósítása

--- Laravel/myapp/resources/views/listings/index.blade.php
+++ Laravel/myapp/resources/views/listings/index.blade.php
@@ -7,7 +7,10 @@
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 @if(count($listings) == 0)
-    <p>No listings found</p>
+    <p>
+        No listings found
+    </p>
+    
 @endif
 @foreach($listings as $listing)
     <x-listing-card :listing="$listing" />
@@ -15,4 +18,7 @@
 @endforeach
 </div>
 
+<div class="mt-6 p-4">
+    {{$listings->links()}}
+</div>
 @endsection
