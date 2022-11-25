3e1d6d19920b844407b447fd0937fe4eb32dd5d6
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2022-11-25 13:00:34 +0100

    Laravel - Lista controller routes

--- Laravel/myapp/routes/web.php
+++ Laravel/myapp/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -15,23 +16,12 @@ use App\Models\Listing;
 |
 */
 //All listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 
 //Single listing
 
-Route::get('/listings/{listing}', function(Listing $listing) {
-
-    return view('listing', [
-        'listing' => $listing
-    ]);
-
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 //User
 Route::get('/user/{user}', [App\Http\Controllers\UserController::class, 'index'])->name('user.show');
