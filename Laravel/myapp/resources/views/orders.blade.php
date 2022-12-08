e63b3f4afca0664f8d9ad496d152a848cbee47b2
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2022-12-08 11:56:31 +0100

    Laravel - Orders-ek megjelenítése

---
+++ Laravel/myapp/resources/views/orders.blade.php
@@ -0,0 +1,62 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container">
+    <div class="card d-lg-flex align-items-lg-center">
+        <div class="card-body">
+            <h4 class="card-title">Placed orders</h4>
+        </div>
+        <div class="card-body text-start d-lg-flex">
+            <div class="table-responsive overflow-visible" style="margin-left: 0px;">
+                <table class="table ">
+                    <thead></thead>
+                    <tr>
+
+                        <th scope="col">Image</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">City</th>
+                        <th scope="col">Adress</th>
+
+                    </tr>
+                    <tbody>
+                    <?php $total = 0;?>
+                    @foreach ($orders as $item)
+                            <?php $listing = $item->getModel();
+                                ?>
+                        <tr>
+                            <td>
+                                <img class="hidden w-10 mr-6 md:block"
+                                     src="{{ $listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png') }}"
+                                     alt="" />
+                            </td>
+                            <td>{{$listing->title}}</td>
+                            <td {{$total += $listing->price}}>{{$listing->price}} Ft</td>
+                            <td>{{$listing->name}}</td>
+                            <td>{{$listing->email}}</td>
+                            <td>{{$listing->city}}</td>
+                            <td>{{$listing->adress}}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <div class="row p-2">
+                    <div class="col">
+                        <label>Total:</label>
+                    </div>
+                    <div class="col" style="text-align: right">
+                        <label >{{$total}}Ft</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+
+
+@endsection
