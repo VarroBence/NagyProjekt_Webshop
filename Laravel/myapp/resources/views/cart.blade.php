d7e71ba96877fc31967375745419a4584bfc020b
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2022-12-08 15:12:35 +0100

    Cart: kis div rendezés

--- Laravel/myapp/resources/views/cart.blade.php
+++ Laravel/myapp/resources/views/cart.blade.php
@@ -17,9 +17,12 @@
                     </div>
                     <div class="card-body text-start d-lg-flex">
                         <div class="table-responsive overflow-visible" style="margin-left: 0px;">
-                            <?php $items = Cart::name('shopping')->getItems(); $i = 1;
+                            <?php
+                                  $items = Cart::name('shopping')->getItems();
+                                  $i = 0;
                                   $total = 0;
-                                  $shipping = 1800;?>
+                                  $shipping = 1800;
+                            ?>
                             @if(empty($items))
                                 <div>
                                     <label class=>Your cart is empty!</label>
@@ -35,25 +38,16 @@
                                     </tr>
                                     <tbody>
                                     @foreach ($items as $item)
-                                            <?php $listing = $item->getModel();?>
                                         <?php $listing = $item->getModel(); $i++;?>
                                         <tr>
-                                            <th class="text-center">{{$i++}}</th>
                                             <th class="text-center">{{$i}}</th>
                                             <td>
                                                 <img class="hidden w-10 mr-6 md:block"
                                                      src="{{ $listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png') }}"
-                                                     alt="" />
                                                      alt=""/>
                                             </td>
-                                            <td>{{$listing->title}}</td>
                                             <td style="width: 20rem; overflow: clip">{{$listing->title}}</td>
                                             <td {{$total += $listing->price}}>{{$listing->price}} Ft</td>
-                                            <td>
-                                                <button type="button" class="btn btn-danger">
-                                                    x
-                                                </button>
-                                            </td>
                                         </tr>
                                     @endforeach
                                         <tr>
@@ -93,28 +87,61 @@
             <div class="col-lg-6 offset-lg-0">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Shipping details</h4>
+                        <h4 class="card-title text-center">
+                            Shipping details
+                        </h4>
                         <form method="POST" action="/cart">
                             @csrf
                             <div class="row">
-                                <div class="col"><label class="form-label">name</label><input class="form-control" type="text" name="name">
+                                <div class="col">
+                                    <label class="form-label">
+                                        name
+                                    </label>
+                                    <input class="form-control" type="text" name="name">
                                     <div class="row">
-                                        <div class="col"><label class="form-label">email</label><input class="form-control" type="email" name="email"></div>
+                                        <div class="col"><label class="form-label">
+                                                email
+                                            </label>
+                                            <input class="form-control" type="email" name="email">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col"><label class="form-label">phone</label><input class="form-control d-lg-flex justify-content-lg-end" type="tel" name="phone"></div>
+                                <div class="col">
+                                    <label class="form-label">
+                                        phone
+                                    </label>
+                                    <input class="form-control d-lg-flex justify-content-lg-end" type="tel" name="phone">
+                                </div>
                             </div>
                             <div class="row">
-                                <div class="col"><label class="form-label">post code</label><input class="form-control d-lg-flex justify-content-lg-end" type="text" name="post_code"></div>
-                                <div class="col"><label class="form-label">city</label><input class="form-control" type="text" name="city"></div>
+                                <div class="col">
+                                    <label class="form-label">
+                                        post code
+                                    </label><input class="form-control d-lg-flex justify-content-lg-end" type="text" name="post_code">
+                                </div>
+                                <div class="col">
+                                    <label class="form-label">
+                                        city
+                                    </label>
+                                    <input class="form-control" type="text" name="city">
+                                </div>
                             </div>
                             <div class="row">
-                                <div class="col"><label class="form-label">address</label><input class="form-control" type="text" name="address"></div>
+                                <div class="col">
+                                    <label class="form-label">
+                                        address
+                                    </label>
+                                    <input class="form-control" type="text" name="address">
+                                </div>
                             </div>
                             <div class="row">
-                                <div class="col d-lg-flex justify-content-lg-end" style="margin-top: 26px;"><button class="btn btn-primary d-lg-flex" type="submit">place order</button></div>
+                                <div class="col d-lg-flex justify-content-center" style="margin-top: 26px;">
+                                    <button class="btn block bg-black text-white py-2 rounded-xl hover:opacity-80 p-2" type="submit">
+                                        Place order
+                                    </button>
+                                </div>
                             </div>
                         </form>
                     </div>
