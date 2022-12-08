a54d160a73fb1fdd2d8a43e665e460f17656d96b
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2022-12-08 15:10:10 +0100

    CartController: add

--- Laravel/myapp/app/Http/Controllers/CartController.php
+++ Laravel/myapp/app/Http/Controllers/CartController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Listing;
 use Jackiedo\Cart\Cart;
 use Illuminate\Http\Request;
 
@@ -10,4 +11,13 @@ class CartController extends Controller
     {
         return view('cart');
     }
+
+    public function add(Listing $listing){
+        $cart = new Cart();
+        $cartItem = $cart -> name('shopping')->addItem([
+            'model' => $listing
+        ]);
+        return back();
+    }
+
 }
