443942636cb291149b1d39b9d1f8322cc13234e3
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-01-17 16:14:51 +0100

    AdminController létrehozva

---
+++ Laravel/myapp/app/Http/Controllers/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use function PHPUnit\Framework\isEmpty;
+
+class AdminController extends Controller
+{
+}
