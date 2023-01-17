9250172f2dc1ee884a82663ce58ef4578ceab665
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-01-17 17:29:44 +0100

    _hero admin only dolgok elrejtése

--- Laravel/myapp/resources/views/partials/_hero.blade.php
+++ Laravel/myapp/resources/views/partials/_hero.blade.php
@@ -5,11 +5,19 @@ class="relative h-72 bg-blue flex flex-col justify-center align-center text-cent
     class="absolute top-0 left-0 w-full h-full opacity-10 bg-no-repeat bg-center"
     style="background-image: url('images/laravel-logo.png')"
 ></div>
-
+        @php
+            $admin = \App\Http\Controllers\AdminController::check(\Illuminate\Support\Facades\Auth::id());
+        @endphp
 <div class="z-10">
+    @if($admin)
+    <h1 class="text-6xl font-bold uppercase text-white">
+        T-Shirt<span class="text-black">Welcome Admin!</span>
+    </h1>
+    @else
     <h1 class="text-6xl font-bold uppercase text-white">
         T-Shirt<span class="text-black">Webshop</span>
     </h1>
+    @endif
     <p class="text-2xl text-black-200 font-bold my-4">
         Find T-shirts for your taste
     </p>
