1cb184b498c267522074676ee7cbe3dce5d98945
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-01-17 16:15:06 +0100

    AdminController check() metódus

--- Laravel/myapp/app/Http/Controllers/AdminController.php
+++ Laravel/myapp/app/Http/Controllers/AdminController.php
@@ -8,4 +8,12 @@ use function PHPUnit\Framework\isEmpty;
 
 class AdminController extends Controller
 {
+    public static function check(int $id): bool
+    {
+        $found = DB::select('select * from users where id = ?', [$id]);
+        if(isEmpty($found)){
+            return false;
+        }
+        return true;
+    }
 }
