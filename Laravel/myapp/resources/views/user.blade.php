eb231d334562cfa04f166b13ea9b5a4994322630
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-01-17 18:20:57 +0100

    User: tábla hozzáadása

--- Laravel/myapp/resources/views/user.blade.php
+++ Laravel/myapp/resources/views/user.blade.php
@@ -5,24 +5,44 @@
     </a>
     <div class="mx-4">
         <x-card class="p-10">
-            <div
-                class="flex flex-col items-center justify-center text-center"
-            >
-                <img
-                    class="w-48 mr-6 mb-6"
-                    src="{{asset('images/no-image.png')}}"
-                    alt=""
-                />
-
+            <div class="flex flex-col items-center justify-center text-center">
                 <h3 class="text-2xl mb-2">{{$user->name}}</h3>
-
-
                 <div class="border border-gray-200 w-full mb-6"></div>
-                <div>
-                    <h3 class="text-3xl font-bold mb-4">
-                        {{$user->name}}
-                    </h3>
-                    <div class="text-lg space-y-6">
+                <div class="table-responsive overflow-visible" style="margin-left: 0px;">
+                    <table class="table ">
+                        <thead></thead>
+                        <tr>
+
+                            <th scope="col">Image</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">City</th>
+                            <th scope="col">Address</th>
+
+                        </tr>
+                        <tbody>
+
+                        <!--
+                            TODO: Ide másold át az orderből a kiiratást meg valahogy csináld meg hogy id alapján kiválogassa
+                                  A jelenlegi user id-ját itt el tudod érni $user->id -val szerintem, minden máshol meg Auth::id() -vel
+                        -->
+
+                        </tbody>
+                    </table>
+                    <div class="row p-2">
+                        <div class="col">
+                            <label>Shipping: 1800 Ft</label>
+                        </div>
+                    </div>
+                    <div class="row p-2">
+                        <div class="col">
+                            <label>Total:</label>
+                        </div>
+                        <div class="col" style="text-align: right">
+                            <label >{{$total}}Ft</label>
+                        </div>
                     </div>
                 </div>
             </div>
